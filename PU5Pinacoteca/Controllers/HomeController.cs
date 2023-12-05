ad474266031ab388744dccd6120e71ec824e0ec2
Yadira Zamarrón Rodríguez <201G0292@rcarbonifera.tecnm.mx>
2023-12-04 23:10:29 -0800

    Add project files.

---
+++ PU5Pinacoteca/Controllers/HomeController.cs
@@ -0,0 +1,55 @@
+﻿using Microsoft.AspNetCore.Authentication;
+using Microsoft.AspNetCore.Mvc;
+
+namespace PU5Pinacoteca.Controllers
+{
+    public class HomeController : Controller
+    {
+        //INYECTAR EL REPOSITORIO
+
+
+        public IActionResult Index()
+        {
+            return View();
+        }
+        public IActionResult Cuadros()
+        {
+            return View();
+        }
+        public IActionResult Pintores()
+        {
+            return View();
+        }
+        public IActionResult VerCuadro()
+        {
+            return View();
+        }
+        public IActionResult VerPintor()
+        {
+            return View();
+        }
+
+
+        //SIN VISTA:
+        [HttpGet]
+        public IActionResult Login()
+        {
+            return View();
+        }
+        //[HttpPost]
+        //public IActionResult Login()
+        //{
+        //    return View();
+        //}
+        public IActionResult Denied()
+        {
+            return View();
+        }
+
+        public IActionResult Logout()
+        {
+            HttpContext.SignOutAsync();
+            return RedirectToAction("Index", "Home");
+        }
+    }
+}
