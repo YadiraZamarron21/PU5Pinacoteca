2116f5fc90f4d9c53afb9658f97ba0cf98bf727e
HectorContrerasD <201g0239@rcarbonifera.tecnm.mx>
2023-12-07 15:47:51 -0600

    Crud Completo

--- PU5Pinacoteca/Areas/Admin/Controllers/CuadrosController.cs
+++ PU5Pinacoteca/Areas/Admin/Controllers/CuadrosController.cs
@@ -14,7 +14,7 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
         private readonly CuadrosRepository cuadrosRepos;
         private readonly Repository<Coleccion> coleccionRepos;
         private readonly Repository<Pintor> pintorRepo;
-        public CuadrosController(CuadrosRepository cuadrosRepository, Repository<Coleccion> coleccionRepository,Repository<Pintor> pintorRepository)
+        public CuadrosController(CuadrosRepository cuadrosRepository, Repository<Coleccion> coleccionRepository, Repository<Pintor> pintorRepository)
         {
             cuadrosRepos = cuadrosRepository;
             coleccionRepos = coleccionRepository;
@@ -25,10 +25,10 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
         {
             AdminVerCuadrosViewModel vm = new()
             {
-                Colecciones = cuadrosRepos.GetAll().GroupBy(x=>x.IdColeccionNavigation).Select(x=> new AdminVerColeccionModel
+                Colecciones = cuadrosRepos.GetAll().GroupBy(x => x.IdColeccionNavigation).Select(x => new AdminVerColeccionModel
                 {
                     Clasificacion = x.Key.Nombre,
-                    Cuadros = cuadrosRepos.GetAll().Where(a=>a.IdColeccion == x.Key.Id).Select(x=> new AdminVerCuadroModel
+                    Cuadros = cuadrosRepos.GetAll().Where(a => a.IdColeccion == x.Key.Id).Select(x => new AdminVerCuadroModel
                     {
                         Id = x.Id,
                         Nombre = x.TituloCuadro,
@@ -47,7 +47,7 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
                 Id = x.Id,
                 Nombre = x.Nombre
             });
-            vm.Pintores = pintorRepo.GetAll().Select(x=> new AdminPintorModel
+            vm.Pintores = pintorRepo.GetAll().Select(x => new AdminPintorModel
             {
                 Id = x.IdPintor,
                 Nombre = x.Nombre
@@ -63,24 +63,24 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
                 {
                     ModelState.AddModelError("", "Sólo se permiten imagenes JPG");
                 }
-                if (vm.Archivo.Length > 500 * 1024)
+                if (vm.Archivo.Length > 5000 * 1024)
                 {
-                    ModelState.AddModelError("", "Sólo se permiten archivos no mayores a 500kb");
+                    ModelState.AddModelError("", "Sólo se permiten archivos no mayores a 5mb");
                 }
             }
             if (string.IsNullOrWhiteSpace(vm.Nombre))
             {
-                ModelState.AddModelError("","Escribe el nombre del cuadro");
+                ModelState.AddModelError("", "Escribe el nombre del cuadro");
             }
             if (string.IsNullOrWhiteSpace(vm.AñoPintado.ToString()))
             {
-                ModelState.AddModelError("","El año no puede estar vacío");
+                ModelState.AddModelError("", "El año no puede estar vacío");
             }
             else
             {
                 if (vm.AñoPintado > DateTime.Now.Year)
                 {
-                    ModelState.AddModelError("","La fecha no puede ser en un futuro");
+                    ModelState.AddModelError("", "La fecha no puede ser en un futuro");
                 }
             }
             if (string.IsNullOrWhiteSpace(vm.Tecnica))
@@ -156,8 +156,86 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
                     IdPintor = cuadro.IdPintor,
                     Nombre = cuadro.TituloCuadro,
                     Tecnica = cuadro.Tecnica
-                
+
                 };
+                vm.Colecciones = coleccionRepos.GetAll().Select(x => new AdminColeccionModel
+                {
+                    Id = x.Id,
+                    Nombre = x.Nombre
+                });
+                vm.Pintores = pintorRepo.GetAll().Select(x => new AdminPintorModel
+                {
+                    Id = x.IdPintor,
+                    Nombre = x.Nombre
+                });
+                return View(vm);
+            }
+        }
+        [HttpPost]
+        public IActionResult Editar(AdminAgregarCuadroViewModel vm)
+        {
+            if (vm.Archivo != null)
+            {
+                if (vm.Archivo.ContentType != "image/jpeg")
+                {
+                    ModelState.AddModelError("", "Sólo se permiten imagenes JPG");
+                }
+                if (vm.Archivo.Length > 5000 * 1024)
+                {
+                    ModelState.AddModelError("", "Sólo se permiten archivos no mayores a 5mb");
+                }
+            }
+            if (string.IsNullOrWhiteSpace(vm.Nombre))
+            {
+                ModelState.AddModelError("", "Escribe el nombre del cuadro");
+            }
+            if (string.IsNullOrWhiteSpace(vm.AñoPintado.ToString()))
+            {
+                ModelState.AddModelError("", "El año no puede estar vacío");
+            }
+            else
+            {
+                if (vm.AñoPintado > DateTime.Now.Year)
+                {
+                    ModelState.AddModelError("", "La fecha no puede ser en un futuro");
+                }
+            }
+            if (string.IsNullOrWhiteSpace(vm.Tecnica))
+            {
+                ModelState.AddModelError("", "Escribe el nombre de la técnica que se usó");
+            }
+            if (string.IsNullOrWhiteSpace(vm.Dimensiones))
+            {
+                ModelState.AddModelError("", "Escriba las dimensiones del cuadro");
+            }
+            if (string.IsNullOrWhiteSpace(vm.Descripcion))
+            {
+                ModelState.AddModelError("", "Escriba la descripción del cuadro");
+            }
+            if (ModelState.IsValid)
+            {
+                var cuadro = new Cuadro()
+                {
+                    Descripcion = vm.Descripcion,
+                    Dimensiones = vm.Dimensiones,
+                    FechaPintado = vm.AñoPintado,
+                    Id = vm.Id,
+                    IdColeccion = vm.IdColeccion,
+                    IdPintor = vm.IdPintor,
+                    Tecnica = vm.Tecnica,
+                    TituloCuadro = vm.Nombre
+
+                };
+                cuadrosRepos.Update(cuadro);
+                if (vm.Archivo != null)
+                {
+                    System.IO.FileStream fs = System.IO.File.Create($"wwwroot/Cuadros/{cuadro.Id}.jpg");
+                    vm.Archivo.CopyTo(fs);
+                    fs.Close();
+                }
+
+                return RedirectToAction("Index");
+            }
             vm.Colecciones = coleccionRepos.GetAll().Select(x => new AdminColeccionModel
             {
                 Id = x.Id,
@@ -169,24 +247,36 @@ namespace PU5Pinacoteca.Areas.Admin.Controllers
                 Nombre = x.Nombre
             });
             return View(vm);
-            }
-        }
-        [HttpPost]
-        public IActionResult Editar()
-        {
-            return View();
         }
 
 
         [HttpGet]
         public IActionResult Eliminar(int id)
         {
-            return View();
+            var cuadro = cuadrosRepos.Get(id);
+            if (cuadro == null)
+            {
+                return RedirectToAction("Index");
+            }
+            return View(cuadro);
         }
         [HttpPost]
-        public IActionResult Eliminar()
+        public IActionResult Eliminar(Cuadro c)
         {
-            return View();
+            var cuadro = cuadrosRepos.Get(c.Id);
+            if (cuadro == null)
+            {
+                return RedirectToAction("Index");
+            }
+            cuadrosRepos.Delete(cuadro);
+            var imagen = $"wwwroot/Cuadros/{cuadro.Id}.jpg";
+            if (System.IO.File.Exists(imagen))
+            {
+                System.IO.File.Delete(imagen);
+            }
+
+            return RedirectToAction("Index");
+
         }
     }
 }
