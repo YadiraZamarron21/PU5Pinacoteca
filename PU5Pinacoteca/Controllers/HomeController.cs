86e154066bd621a31184e7ebf958e9b0014dcbf5
CarlosCuautleZac <191g0235@rcarbonifeta.tecnm.mx>
2023-12-07 15:04:05 -0600

    Cambios vista

--- PU5Pinacoteca/Controllers/HomeController.cs
+++ PU5Pinacoteca/Controllers/HomeController.cs
@@ -1,12 +1,24 @@
 ﻿using Microsoft.AspNetCore.Authentication;
 using Microsoft.AspNetCore.Mvc;
+using PU5Pinacoteca.Models.Entities;
+using PU5Pinacoteca.Models.ViewModels;
+using PU5Pinacoteca.Repositories;
 
 namespace PU5Pinacoteca.Controllers
 {
     public class HomeController : Controller
     {
         //INYECTAR EL REPOSITORIO
+        private readonly CuadrosRepository cuadrosRepository;
+        private readonly Repository<Coleccion> coleccionRepository;
+        private readonly Repository<Pintor> pintorRepository;
 
+        public HomeController(CuadrosRepository cuadrosRepository, Repository<Coleccion> coleccionRepository, Repository<Pintor> pintorRepository)
+        {
+            this.cuadrosRepository = cuadrosRepository;
+            this.coleccionRepository = coleccionRepository;
+            this.pintorRepository = pintorRepository;
+        }
 
         public IActionResult Index()
         {
@@ -14,7 +26,20 @@ namespace PU5Pinacoteca.Controllers
         }
         public IActionResult Cuadros()
         {
-            return View();
+            CuadrosViewModel vm = new()
+            {
+                Colecciones = cuadrosRepository.GetAll().GroupBy(x => x.IdColeccionNavigation).Select(x => new ColeccionModel
+                {
+                    Clasificacion = x.Key.Nombre,
+                    Cuadros = cuadrosRepository.GetAll().Where(a => a.IdColeccion == x.Key.Id).Select(x => new CuadroModel
+                    {
+                        Id = x.Id,
+                        Nombre = x.TituloCuadro,
+                        NombrePintor = x.IdPintorNavigation.Nombre
+                    })
+                })
+            };
+            return View(vm);
         }
         public IActionResult Pintores()
         {
